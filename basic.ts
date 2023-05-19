ac43fe62c3bbd4e536e615b2f2cca0687fe97431
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-05-19 09:25:11 +0545

    parsing-ts

--- basic.ts
+++ basic.ts
@@ -1,22 +1,24 @@
 import z from "zod";
 
-const schema = z.object({
-  name: z.string({
-    required_error: "Name is required",
-    invalid_type_error: "Name must be a string",
-  }),
-  age: z.number(),
-  isAlive: z.boolean(),
-  hobbies: z.array(z.string()),
-  address: z.object({
-    street: z.string(),
-  }),
-});
+const schema = z
+  .object({
+    name: z.string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    }),
+    age: z.number(),
+    isAlive: z.boolean(),
+    hobbies: z.array(z.string()),
+    address: z.object({
+      street: z.string(),
+    }),
+  })
+  .passthrough();
 
 type CreateUserInput = z.infer<typeof schema>;
 
 function createUser(props: CreateUserInput) {
-  const result = schema.parse(props);
+  const result = schema.safeParse(props);
 
   return result;
 }
